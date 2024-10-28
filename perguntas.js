c31cbdd1fdea39e441f280a3c69d22de198c6a91
OlMngckas <164903017+OlMngckas@users.noreply.github.com>
2024-10-28 12:20:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,46 @@
+criaCartao(
+    'Quem eu sou',
+    'Quem é voce?',
+    'meu nome é olga tenho 16 anos  estudo no colégio morski e meu esporte favorito é jogar vôlei'
+)
+
+criaCartao(
+    'sobre minha familia',
+    'como minha familia é?',
+    'minha família é composta por 4 pessoas meu pai minha mãe e minha irmãzinha meus pais trabalham ambos são formados em direito'
+)
+
+criaCartao(
+    '3-moradia',
+    'Onde mora?',
+    'onde moro; rua trajano ferreira dos santos,número 78, bairro azaleia'
+)
+
+criaCartao(
+    '4-estudos',
+    'onde estudo?',
+    'onde estudo: colégio mario evaldo morski o colégio foi fundada em 1994  em homenagem ao professor mario evaldo morski na atualidade é composta por ensino médio e fundamental 2'
+)
+criaCartao(
+    '5-turma',
+    'comoé minha turma?',
+    'minha turma: suportável, tem pessoas que não sabem se comportar e nao sao bons alunos '
+)
+
+criaCartao(
+    '6-faculdade',
+    'Qual faculde vou fazer?',
+    'faculdade: três curso penso em fazer começando com pedagogia depois veterinária e talvez uma medicina'
+)
+
+criaCartao(
+    '7-cidade',
+    'como é minha cidade?',
+    'pinhão e uma cidade confortável e aconchegante, e localizada no interior com uma população média '
+)
+
+criaCartao(
+    '8-sonho',
+    'qual seu sonho?',
+    'meu sonho é ser jogadora de vôlei e ganhar uma medalha olímpica'
+)
